253525d10e89413a7005f76853608b000ced9b6e
Isaiah Vickers <isaiah.vickers@outlook.com>
2023-10-23 19:24:11 -0400

    Functions in Redo

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
-from website import app
+import getpass
+import bcrypt
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+def add_login_info():
+    login_info = {}
+
+    url = input("Add the website link: ")
+    title = input("Add the title: ")
+    username = input("Add username: ")
+    email = input("Add email: ")
+    password = input("Add password: ")
+
+    login_info['url'] = url
+    login_info['title'] = title if title else "No Title"
+
+    login_info['username'] = username if username else email
+
+    login_info['email'] = email
+
+    encrypted_password = encrypt_password(password)
+
+    login_info['password'] = encrypted_password
+
+
+    return login_info
+
+def encrypt_password(password):
+
+    binary_password = password.encode('utf-8')
+
+    salt = bcrypt.gensalt()
+    encrypted_password = bcrypt.hashpw(binary_password, salt)
+
+    return encrypted_password
+
+
+information = add_login_info()
+
+print(information)
--- website/routes.py
+++
@@ -1,2 +0,0 @@
-from flask import render_template, url_for, flash, redirect
-from website import app
