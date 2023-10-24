d1a4a9a007c8fb66b5904fd509fbd2127c128210
Isaiah Vickers <isaiah.vickers@outlook.com>
2023-10-24 10:04:21 -0400

    create interface

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import getpass
 import bcrypt
 
+
+class User:
+
+    """Characteristics of a User using the Password Manager."""
+
+
 def add_login_info():
     login_info = {}
 
@@ -10,23 +16,22 @@ def add_login_info():
     email = input("Add email: ")
     password = input("Add password: ")
 
-    login_info['url'] = url
-    login_info['title'] = title if title else "No Title"
+    login_info["url"] = url
+    login_info["title"] = title if title else "No Title"
 
-    login_info['username'] = username if username else email
+    login_info["username"] = username if username else email
 
-    login_info['email'] = email
+    login_info["email"] = email
 
     encrypted_password = encrypt_password(password)
 
-    login_info['password'] = encrypted_password
-
+    login_info["password"] = encrypted_password
 
     return login_info
 
-def encrypt_password(password):
 
-    binary_password = password.encode('utf-8')
+def encrypt_password(password):
+    binary_password = password.encode("utf-8")
 
     salt = bcrypt.gensalt()
     encrypted_password = bcrypt.hashpw(binary_password, salt)
